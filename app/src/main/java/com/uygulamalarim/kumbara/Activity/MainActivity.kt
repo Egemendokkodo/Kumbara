edf2a07154f396128a4e37e1384a5c1ccb976a5b
Egemen Sevgi <egemen2001sevgi@hotmail.com>
2023-03-20 15:28:56 +0300

    created splashscreen with lottie animation

--- app/src/main/java/com/uygulamalarim/kumbara/Activity/MainActivity.kt
+++ app/src/main/java/com/uygulamalarim/kumbara/Activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.uygulamalarim.kumbara
+package com.uygulamalarim.kumbara.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.uygulamalarim.kumbara.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/uygulamalarim/kumbara/Activity/SplashScreen.kt
@@ -0,0 +1,29 @@
+package com.uygulamalarim.kumbara.Activity
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.uygulamalarim.kumbara.R
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+
+        animationView.alpha = 0f
+        animationView.animate().setDuration(1500).alpha(1f).withEndAction{
+               val intent = Intent(this@SplashScreen, MainActivity::class.java)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            startActivity(intent)
+            finish()
+            }
+        }
+
+
+
+}
