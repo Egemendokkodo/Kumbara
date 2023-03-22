a2591f179c3122461c1a4e2c3f83a37a6d2df48d
Egemen Sevgi <egemen2001sevgi@hotmail.com>
2023-03-22 16:24:55 +0300

    created fragment and layout for add new goal fab button

--- app/src/main/java/com/uygulamalarim/kumbara/Fragments/AddFragment.kt
+++ app/src/main/java/com/uygulamalarim/kumbara/Fragments/AddFragment.kt
@@ -5,12 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.uygulamalarim.kumbara.Activity.MainActivity
 import com.uygulamalarim.kumbara.R
-import kotlinx.android.synthetic.main.fragment_landing.*
-import kotlinx.android.synthetic.main.fragment_main.*
 
-class MainFragment : Fragment() {
+
+class AddFragment : Fragment() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,22 +21,11 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_add, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        newgoalbtn.setOnClickListener{
-            changeFragment(AddFragment())
-        }
 
     }
-
-    private fun changeFragment(fragment: Fragment) {
-        val fragmentTransaction = getParentFragmentManager().beginTransaction()
-        fragmentTransaction.replace(R.id.frameLayout, fragment)
-        fragmentTransaction.commit()
-    }
-
-
 }
